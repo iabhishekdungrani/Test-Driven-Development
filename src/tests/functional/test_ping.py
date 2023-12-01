72de616ffd1b8654db950fa4c9cd1176f81ab1c5
iabhishekdungrani <iabhishekdungrani@gmail.com>
2023-12-01 14:31:02 -0500

    Pytest Setup
    Tests, Given-When-Then, Selecting Tests

---
+++ src/tests/functional/test_ping.py
@@ -0,0 +1,13 @@
+import json
+
+
+def test_ping(test_app):
+    #Given
+    client = test_app.test_client()
+    #when
+    resp = client.get('/ping')
+    data = json.loads(resp.data.decode())
+    #then
+    assert resp.status_code == 200
+    assert 'pong' in data['message']
+    assert 'success' in data['status']
